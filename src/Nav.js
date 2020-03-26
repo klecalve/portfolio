66f61f85a79ea71bd1b4041ef93b23807f939a78
Kevin Le Calvé <KalvinCleeve@users.noreply.github.com>
2020-03-26 17:13:00 +0100

    fix

---
+++ src/Nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
+
+export default class Nav extends React.Component {
+  render() {
+    return (
+      <nav>
+        <h3>LOGO</h3>
+        <ul className="nav-links">
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/header">
+            <li>Something</li>
+          </Link>
+        </ul>
+      </nav>
+    );
+  }
+}
